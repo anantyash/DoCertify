f6bba6581b32b6ecdae7f00253f406a5a85e1fe0
Yash <anantyash.2710@gmail.com>
2024-09-29 00:05:02 +0530

    Config Drawer Navigation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import './gesture-handler';
 import {name as appName} from './app.json';
 import DoCertify from './App';
 
