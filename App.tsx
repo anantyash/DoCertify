5b432fc033d09e1c974b10d457bc9d61db6e05ac
Yash <anantyash.2710@gmail.com>
2024-09-29 00:03:51 +0530

    Add Home Screen and update it

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import WelcomeScreen from './components/WelcomeScreen'
 import LoginPage from './components/LoginPage'
 import { NavigationContainer } from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Home from './screens/Home'
 
 
 export type RootStackParamList = {
  WelcomeScreen: undefined,
- LoginPage: undefined
+ LoginPage: undefined,
+ Home : undefined
 };
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -18,8 +20,13 @@ export default function DoCertify(){
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen}/>
-        <Stack.Screen name='LoginPage' component={LoginPage} />
+        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen} options={{
+          headerShown:false
+        }}/>
+        <Stack.Screen name='LoginPage' component={LoginPage} options={{
+          headerShown:false
+        }} />
+        <Stack.Screen name='Home' component={Home}/>
       </Stack.Navigator>
 
     </NavigationContainer>
